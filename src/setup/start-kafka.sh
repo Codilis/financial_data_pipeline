0f70e9d83776d82c77fde16fb7b7b9f9165afda8
Palash Jain <palash.j22@gmail.com>
2024-08-18 16:12:04 +0530

    removed server1 and 2 and updated server.properties file

--- src/setup/start-kafka.sh
+++ src/setup/start-kafka.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
+set -e
+
 zookeeper-server-start.sh /opt/kafka/config/zookeeper.properties &
 echo "Zookeeper Started";
 kafka-server-start.sh /opt/kafka/config/server.properties &
 echo "Server Started 1";
-kafka-server-start.sh /opt/kafka/config/server-1.properties &
-echo "Server Started 2";
-kafka-server-start.sh /opt/kafka/config/server-2.properties &
-echo "Server Started 3";
 kafka-topics.sh --create --topic transaction_stream --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1;
+nohup spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 /src/kafka_streams/consumer.py &
