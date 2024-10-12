82de17ee61b3c064d319b5116e88911dc36b02dc
Palash Jain <palash.j22@gmail.com>
2024-10-12 19:35:15 +0530

    added gold_zone dag, add dim_client model

--- src/airflow_dags/run_dbt.py
+++ src/airflow_dags/run_dbt.py
@@ -13,8 +13,8 @@ default_args = {
     'retry_delay': timedelta(hours=1)
 }
 
-dbt_dag = DAG(
-    dag_id='dbt_run_dag',
+data_pipeline = DAG(
+    dag_id='data_pipeline_dag',
     default_args=default_args,
     description='Run dbt for all the tables!',
     schedule_interval=timedelta(days=1),
@@ -23,15 +23,20 @@ dbt_dag = DAG(
 run_transactions_consumer = BashOperator(
     task_id='run_transactions_consumer',
     bash_command='cd /shared_volume && spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 /src/kafka_streams/consumer.py',
-    dag=dbt_dag
+    dag=data_pipeline
 )
 
 
 load_silver_zone = BashOperator(
     task_id='load_silver_zone',
     bash_command='cd /shared_volume && $SPARK_HOME/sbin/stop-thriftserver.sh && spark-submit /src/load_silver_zone/main.py && $SPARK_HOME/sbin/start-thriftserver.sh --master local[*]',
-    dag=dbt_dag
+    dag=data_pipeline
 )
 
+load_gold_zone = BashOperator(
+    task_id='load_gold_zone',
+    bash_command='cd /src/gold_zone_dbt && dbt run',
+    dag=data_pipeline
+)
 
-run_transactions_consumer >> load_silver_zone
+run_transactions_consumer >> load_silver_zone >> load_gold_zone
